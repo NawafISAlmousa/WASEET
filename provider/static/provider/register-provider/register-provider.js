95d2b9eb7498ba6d967241c20b51b852f8b3a256
NawafISAlmousa <nawaf.i.s.m2@gmail.com>
2024-10-21 13:02:12 +0300

    Added password matching claus to provider register and fixed js file caching issue

--- provider/static/provider/register-provider/register-provider.js
+++ provider/static/provider/register-provider/register-provider.js
@@ -1,3 +1,6 @@
+const checkbox = document.getElementById('showpass');
+const pass = document.getElementById('password');
+const confirmpass = document.getElementById('confirm-password');
 function previewLogo(event) {
     const [file] = event.target.files;
     if (file) {
@@ -10,22 +13,40 @@ function previewLogo(event) {
 
 document.getElementById('provider-form').addEventListener('submit', async (e) => {
     e.preventDefault();
-    
-    const formData = new FormData(e.target);
-
-    try {
-      const response = await fetch('/provider/registerProvider/', {
-        method: 'POST',
-        body: formData,
-      });
-
-      if (response.ok) {
-        alert('Provider information submitted successfully!');
-        e.target.reset();
-      } else {
-        alert('Error submitting provider information.');
+    if(document.getElementById('password').value === document.getElementById('confirm-password').value){
+      
+      const formData = new FormData(e.target);
+      
+      try {
+        const response = await fetch('/provider/registerProvider/', {
+          method: 'POST',
+          body: formData,
+        });
+      
+        if (response.ok) {
+          alert('Provider information submitted successfully!');
+          e.target.reset();
+          window.location.href = loginurl;
+        } else {
+          alert('Error submitting provider information.');
+        }
+      } catch (error) {
+        console.error('Error:', error);
       }
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  });
+  }else {
+    alert('Password and Confirm Passord Do Not Match!')
+  }  
+});   
+
+
+
+
+checkbox.addEventListener('click', function(){
+  if (checkbox.checked){
+    pass.type = 'text';
+    confirmpass.type = 'text';
+  } else{
+    pass.type = 'password';
+    confirmpass.type = 'password';
+  }
+})
